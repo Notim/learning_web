5286db82e69e55ca8286ced0d7f3d3cdc30c177d
Notim_ <paulino.joaovitor@yahoo.com.br>
2018-12-03 19:08:23 -0200

    Up

---
+++ ASP .NET CORE/Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NET_CORE.ConsoleProgram {
+    public class Anytype {
+        public string  name { get; set; } 
+        public DateTime  dateBirth { get; set; }
+        public string  work { get; set; } 
+    }
+    public class Program {
+        public static void Main(string[] args) {
+            var obj = new Anytype { 
+                name = "Joao vitor paulino martins"
+                , dateBirth = DateTime.Now
+                , work = "Developer"
+            };
+
+            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine(obj.name);
+            Console.WriteLine(obj.dateBirth.ToString());
+            Console.WriteLine(obj.work);
+        }
+    }
+}
